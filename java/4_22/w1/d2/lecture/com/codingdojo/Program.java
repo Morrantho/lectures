3a924881b81a215ab9c89755ca58a3d05be7ec76
morrantho <morrantho@gmail.com>
2022-04-05 09:05:46 -0400

    added w1d2 lecture plan

---
+++ java/4_22/w1/d2/lecture/com/codingdojo/Program.java
@@ -0,0 +1,98 @@
+package com.codingdojo;
+/*****************************************************************************/
+/* Morning Lecture: */
+/*****************************************************************************/
+
+/* What is OOP / What is it's purpose?: */
+
+/*
+	
+*/
+
+/* What is a class?: */
+
+/*
+
+*/
+
+/* What are Objects?: */
+
+/*
+
+*/
+
+/* What is a member variable?: */
+
+/*
+
+*/
+
+/* What is a method?: */
+
+/*
+
+*/
+
+/* What is a constructor / what is it's purpose?: */
+
+/*
+
+*/
+
+/* What does the new keyword do?: */
+
+/*
+
+*/
+
+/*****************************************************************************/
+/* Afternoon Lecture: */
+/*****************************************************************************/
+
+/* What is an access modifier?: */
+
+/*
+
+*/
+
+/* What does the public keyword do?: */
+
+/*
+
+*/
+
+/* What does the private keyword do?: */
+
+/*
+
+*/
+
+/* What does the static keyword do?: */
+
+/*
+
+*/
+
+/* What is method overloading?: */
+
+/*
+
+*/
+
+/* What does the "this" keyword do?: */
+
+/*
+	A way to refer to each unique instance of a class within the class itself.
+*/
+
+/*****************************************************************************/
+/* Make a grocery cart app together! */
+/*****************************************************************************/
+
+public class Program
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+};
---
+++ java/4_22/w1/d2/lecture/com/codingdojo/run.sh
@@ -0,0 +1,3 @@
+# Tells java where to find our source code
+# before attempting to run the program.
+javac Program.java && java -cp ../../ com/codingdojo/Program
