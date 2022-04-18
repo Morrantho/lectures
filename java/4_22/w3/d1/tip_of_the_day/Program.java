fecaff8f74b836672ca46a09c973391a53708392
morrantho <morrantho@pop-os.localdomain>
2022-04-17 21:47:14 -0400

    lecture questions

---
+++ java/4_22/w3/d1/tip_of_the_day/Program.java
@@ -0,0 +1,32 @@
+import java.util.function.Function;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/* Java has lambdas too you know. */
+
+public class Program
+{
+	@SuppressWarnings("unchecked")
+	public static void Iterate(ArrayList<String> arr,Function fn)
+	{
+		for(String str:arr) fn.apply(str);
+	}
+
+	public static void main(String[] args)
+	{
+		ArrayList<String> waltons=new ArrayList<>();
+		waltons.add("JimBob");
+		waltons.add("Mary Ellen");
+		waltons.add("JohnBoy");
+		waltons.add("Liv");
+		waltons.add("John");
+		waltons.add("Esther");
+		waltons.add("Jason");
+		waltons.add("Elizabeth");
+
+		Iterate(waltons,(walton)->{
+			System.out.println(walton);
+			return null;
+		});
+	}
+};
