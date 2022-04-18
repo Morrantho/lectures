d82cebeefe9498db3059e056b01764fa000e1d5d
morrantho <morrantho@pop-os.localdomain>
2022-04-17 22:52:37 -0400

    better 404 page

--- java/4_22/w3/d1/src/main/java/com/codingdojo/controllers/RootController.java
+++ java/4_22/w3/d1/src/main/java/com/codingdojo/controllers/RootController.java
@@ -1,16 +1,26 @@
 package com.codingdojo.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
-public class RootController
+public class RootController implements ErrorController
 {
 	@GetMapping("")
 	public String Root()
 	{
 		return "redirect:/song";
+	}
+
+	@GetMapping("/error")
+	public String handleError(HttpServletRequest req,Model model)
+	{
+		return "404";
 	}
 };
