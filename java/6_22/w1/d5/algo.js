2834b228c327c32763f9a74d725e007bac44fbb4
morrantho <morrantho@gmail.com>
2022-06-10 11:20:22 -0400

    -

--- java/6_22/w1/d5/algo.js
+++ java/6_22/w1/d5/algo.js
@@ -27,20 +27,18 @@ class List
 	/* Add all nodes of the supplied list to this list */
 	Concat(other_list)
 	{
-		/* Your Code Here */
 	}
 
 	/* Finds the node with the smallest value and moves it to the front. */
 	MinToFront()
 	{
-		/* Your Code Here */
+
 	}
 
 	/* Split our list into two lists, where the second list starts with the node */
 	/* that has the given value */
 	Split(value)
 	{
-		/* Your Code Here */
 	}
 };
 /*****************************************************************************/
@@ -53,22 +51,22 @@ let list2=new List();
 
 /* ( Use your solution from day 1 for this to work! ) */
 /* Remember, it just shoves all of these into our list! */
-list.PushBackN([765,234,545,112]);
-list2.PushBackN([65,567,433,656]);
+// list.PushBackN([765,234,545,112]);
+// list2.PushBackN([65,567,433,656]);
 
-list=list.Concat(list2);
-list.Iterate();
+// list=list.Concat(list2);
+// list.Iterate();
 /*
 	Expected:765->234->545->112->65->567->433->656
 */
-list.MinToFront();
-list.Iterate();
+// list.MinToFront();
+// list.Iterate();
 /*
 	Expected:65->765->234->545->112->567->433->656
 */
-let split_list=list.Split(545);
-list.Iterate();
-split_list.Iterate();
+// let split_list=list.Split(545);
+// list.Iterate();
+// split_list.Iterate();
 /*
 	Expected:65->765->234->
 	Expected:545->112->567->433->656
